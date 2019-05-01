34867a310fe03e994729ab81d9493faaf62b2184
Rafał Kucharski <rafal.kucharski@hotmail.com>
2019-05-01 16:32:13 +0200

    Clear form

---
+++ music/music/Models/Music/MusicRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace music.Models.Music
+{
+    public class MusicRequest
+    {
+        public string Title { get; set; }
+    }
+}
